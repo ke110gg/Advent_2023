8a3e2084ec04e27d6212d8c3a4b87ede14bef704
Ben Kellogg <bkellogg5497@gmail.com>
2023-12-17 15:21:15 -0800

    Day 9 part 2

--- advent/bin/day_8.ml
+++ advent/bin/day_8.ml
@@ -1,7 +1,7 @@
 open Core
 module StringMap = Map.Make (String)
 
-type direction =
+type direction_type =
   | RIGHT
   | LEFT
 
@@ -25,7 +25,14 @@ let rec parse_map lines map =
     parse_map body (StringMap.set map ~key:node ~data:(left_node, right_node))
 ;;
 
-let rec run_iteration directions map acc current_point =
+let part_2_comparator string =
+  let pos = String.get string 2 in
+  Char.equal pos 'Z'
+;;
+
+let part_1_comparator string = String.equal string "ZZZ"
+
+let rec run_iteration directions map acc current_point comparator =
   match directions with
   | [] -> current_point, acc
   | d :: directions ->
@@ -39,16 +46,27 @@ let rec run_iteration directions map acc current_point =
       | RIGHT -> right_node
       | LEFT -> left_node
     in
-    if String.equal current_point "ZZZ"
+    if comparator current_point
     then current_point, acc + 1
-    else run_iteration directions map (acc + 1) current_point
+    else run_iteration directions map (acc + 1) current_point comparator
 ;;
 
-let rec run_route directions map acc current_point =
-  let current_point, acc = run_iteration directions map acc current_point in
-  match current_point with
-  | "ZZZ" -> acc
-  | _ -> run_route directions map acc current_point
+let rec run_route directions map acc current_point comparator =
+  let current_point, acc = run_iteration directions map acc current_point comparator in
+  if comparator current_point
+  then acc
+  else run_route directions map acc current_point comparator
+;;
+
+let rec gcd a b = if b = 0 then a else gcd b (a % b)
+
+let rec lcm nums =
+  match nums with
+  | [] -> raise (Invalid_argument "ahh")
+  | a :: [] -> a
+  | a :: b :: body ->
+    let new_list = [ a / gcd a b * b ] @ body in
+    lcm new_list
 ;;
 
 let lines = Advent.Advent_tools.read_lines "./input/puzzle8.txt"
@@ -59,5 +77,16 @@ let directions, map =
   | [] -> [], StringMap.empty
 ;;
 
-let result = run_route directions map 0 "AAA"
-let () = Fmt.pr "Result: %d@." result
+let starting_points =
+  StringMap.fold map ~init:[] ~f:(fun ~key ~data:_ acc ->
+    if Char.( <> ) (String.get key 2) 'A' then acc else acc @ [ key ])
+;;
+
+let result = run_route directions map 0 "AAA" part_1_comparator
+let () = Fmt.pr "result: %d\n" result
+
+let result =
+  List.map starting_points ~f:(fun p -> run_route directions map 0 p part_2_comparator)
+;;
+
+let () = lcm result |> Fmt.pr "result: %d\n"
